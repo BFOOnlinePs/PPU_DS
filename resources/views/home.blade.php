9fa2e82a7324c9ed75bcc65b4518aea3ff6f8e32
AbuMaraqa <maraqamohamad@outlook.sa>
2023-10-05 12:39:42 +0300

    dashboard seactions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,36 @@
 @extends('layouts.app')
-
+@section('title')
+    الرئيسية
+@endsection
+@section('header_title')
+    الرئيسية
+@endsection
+@section('header_title_link')
+    الرئيسية
+@endsection
+@section('header_link')
+    الرئيسية
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="col-sm-12">
+    <div class="card">
+        <div class="card-header">
+            <h5></h5>
+            {{-- <div class="setting-list">
+                <ul class="list-unstyled setting-option">
+                    <li>
+                        <div class="setting-primary"><i class="icon-settings"></i></div>
+                    </li>
+                    <li><i class="view-html fa fa-code font-primary"></i></li>
+                    <li><i class="icofont icofont-maximize full-card font-primary"></i></li>
+                    <li><i class="icofont icofont-minus minimize-card font-primary"></i></li>
+                    <li><i class="icofont icofont-refresh reload-card font-primary"></i></li>
+                    <li><i class="icofont icofont-error close-card font-primary"> </i></li>
+                </ul>
+            </div> --}}
+        </div>
+        <div class="card-body">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
         </div>
     </div>
 </div>
