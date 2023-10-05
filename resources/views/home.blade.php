01664db4a6eb97ccf78a6d64ff23db77f5950752
AbuMaraqa <maraqamohamad@outlook.sa>
2023-10-05 14:58:36 +0300

    dashboard seactions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,26 +12,16 @@
     الرئيسية
 @endsection
 @section('content')
-<div class="col-sm-12">
+{{-- <div class="col-sm-12">
     <div class="card">
         <div class="card-header">
             <h5></h5>
-            {{-- <div class="setting-list">
-                <ul class="list-unstyled setting-option">
-                    <li>
-                        <div class="setting-primary"><i class="icon-settings"></i></div>
-                    </li>
-                    <li><i class="view-html fa fa-code font-primary"></i></li>
-                    <li><i class="icofont icofont-maximize full-card font-primary"></i></li>
-                    <li><i class="icofont icofont-minus minimize-card font-primary"></i></li>
-                    <li><i class="icofont icofont-refresh reload-card font-primary"></i></li>
-                    <li><i class="icofont icofont-error close-card font-primary"> </i></li>
-                </ul>
-            </div> --}}
         </div>
         <div class="card-body">
 
         </div>
     </div>
-</div>
+</div> --}}
+
+Mohamad
 @endsection
