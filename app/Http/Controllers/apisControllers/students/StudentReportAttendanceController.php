9adfcf4e0e9709e7ad3276588e774ae74ae9707f
Aseel-Mohtaseb <aseelamermohtaseb@gmail.com>
2023-11-02 13:09:27 +0200

    student submit only one report

--- app/Http/Controllers/apisControllers/students/StudentReportAttendanceController.php
+++ app/Http/Controllers/apisControllers/students/StudentReportAttendanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\apisControllers\students;
 use App\Http\Controllers\Controller;
 use App\Models\StudentAttendance;
 use App\Models\StudentReport;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class StudentReportAttendanceController extends Controller
@@ -12,18 +13,37 @@ class StudentReportAttendanceController extends Controller
     public function getStudentReportsWithAttendance(Request $request)
     {
         $student_company_id = $request->input('student_company_id');
-        $attendance = StudentAttendance::where('sa_student_company_id', $student_company_id)->get();
+        $attendance = StudentAttendance::where('sa_student_company_id', $student_company_id)
+            ->whereDate('sa_in_time', Carbon::now('Asia/Gaza')->toDateString())
+            ->whereDoesntHave('report')
+            ->get();
+            // same as:
+            // ->where('sa_in_time', '>=', Carbon::now('Asia/Gaza')->toDateString())
+            // for more than one
+            // ->where('sa_in_time', '>=', Carbon::now('Asia/Gaza')->subDays(2))
 
-        $reports = StudentReport::whereHas('attendance', function ($query) use ($student_company_id){
+
+
+        // $reports = StudentReport::whereHas('attendance', function ($query) use ($student_company_id) {
+        //     $query->where('sa_student_company_id', $student_company_id);
+        // })->with('attendance:sa_id,sa_in_time')->get(); // to return the date and print the day of attendance not report
+
+        $reports = StudentReport::whereHas('attendance', function ($query) use ($student_company_id) {
             $query->where('sa_student_company_id', $student_company_id);
-        })->get();
+        })->with(['attendance' => function ($query) {
+            $query->select('sa_id', 'sa_in_time');
+        }])->get();
 
-        if ($attendance->isEmpty()) {
-            return response()->json([
-                'status' => false,
-                'message' => 'لم يسجل الطالب اي حضور مؤخرا'
-            ], 200);
-        }
+        $today = Carbon::now('Asia/Gaza')->toDateString();
+
+        $reports->each(function ($report) use ($today) {
+            $attendance = $report->attendance;
+
+            if ($attendance) {
+                $attendanceDate = Carbon::parse($attendance->sa_in_time)->toDateString();
+                $attendance->is_same_day = $attendanceDate === $today;
+            }
+        });
 
 
         return response()->json([
--- app/Http/Controllers/apisControllers/students/studentReportController.php
+++ app/Http/Controllers/apisControllers/students/studentReportController.php
@@ -50,7 +50,8 @@ class studentReportController extends Controller
         if ($request->hasFile('sr_attached_file')) {
             $file = $request->file('sr_attached_file');
             $folderPath = 'student_reports';
-            $fileName = time() . '_' . uniqid() . '.' . $file->getClientOriginalName();
+            // $fileName = time() . '_' . uniqid() . '.' . $file->getClientOriginalName();
+            $fileName = time() . '_' . uniqid();
             $request->file('sr_attached_file')->storeAs($folderPath, $fileName, 'public');
 
             $studentReport->update([
@@ -61,6 +62,62 @@ class studentReportController extends Controller
         return response()->json([
             'status' => true,
             'message' => 'تم تسليم التقرير بنجاح',
+            'student_report' => $studentReport,
+        ], 200);
+    }
+
+
+    public function studentEditReport(Request $request)    {
+        $validator  = Validator::make($request->all(), [
+            'sr_id' => 'required',
+            'sr_report_text' => 'required',
+            'sr_attached_file' => 'nullable|file|mimes:jpg,jpeg,png,svg,pdf,doc,docx,xls,xlsx,ppt,pptx,odt,ods,odp,mp3,mp4,csv,xlsx,wmv,ai,psd,wmv', // .txt accept other things
+            'sr_submit_longitude' => 'required|string',
+            'sr_submit_latitude' => 'required|string',
+        ],[
+            'sr_id.required' => 'يجب ادخال رقم التقرير',
+            'sr_report_text.required' => 'يجب ادخال نص التقرير',
+            'sr_submit_longitude.required' => 'يجب ادخال الموقع',
+            'sr_submit_latitude.required' => 'يجب ادخال الموقع',
+            'sr_attached_file.mimes' =>'صيغة الملف غير مدعومة، جرب ملفًا آخر',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => $validator->errors()->first(),
+            ], 200);
+        }
+
+        $studentReport = StudentReport::where('sr_id', $request->input('sr_id'))->first();
+
+        if(!$studentReport){
+            return response()->json([
+                'status' => false,
+                'message' => 'رقم التقرير غير موجود',
+            ], 200);
+        }
+
+        $studentReport->update([
+            'sr_report_text' => $request->input('sr_report_text'),
+            'sr_submit_longitude' => $request->input('sr_submit_longitude'),
+            'sr_submit_latitude' => $request->input('sr_submit_latitude'),
+        ]);
+
+        if ($request->hasFile('sr_attached_file')) {
+            $file = $request->file('sr_attached_file');
+            $folderPath = 'student_reports';
+            $fileName = time() . '_' . uniqid();
+            $request->file('sr_attached_file')->storeAs($folderPath, $fileName, 'public');
+
+            $studentReport->update([
+                'sr_attached_file' => $fileName,
+            ]);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'تم تحديث التقرير بنجاح',
             'student_report' => $studentReport,
         ], 200);
     }
