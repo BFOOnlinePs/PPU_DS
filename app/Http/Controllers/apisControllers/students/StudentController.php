7d0a68714ab7eea1e8fda3d75f7673b4f8d42a14
Aseel-Mohtaseb <aseelamermohtaseb@gmail.com>
2023-11-06 14:25:41 +0200

    return more data for trainings

--- app/Http/Controllers/apisControllers/students/StudentController.php
+++ app/Http/Controllers/apisControllers/students/StudentController.php
@@ -15,15 +15,51 @@ class StudentController extends Controller
     {
         $companies = StudentCompany::where('sc_student_id', auth()->user()->u_id)
             ->with('company')
-            // ->with('companyBranch.companies')
-            // ->with('companyBranch.manager')
+            ->with(['userMentorTrainer' => function ($query) {
+                $query->select('u_id', 'name');
+            }])
+            ->with(['userAssistant' => function ($query) {
+                $query->select('u_id', 'name');
+            }])
             ->get();
+
+        // ->with(['companyBranch.manager' => function ($query) {
+        //     $query->select('u_id', 'name');
+        // }])
 
         if ($companies->isEmpty()) {
             return response()->json([
                 'message' => 'لا يوجد تسجيل للطالب في اي شركة حاليا'
             ], 200);
         }
+
+        // in future:
+        // $companies = StudentCompany::where('sc_student_id', auth()->user()->u_id)
+        //     ->with(['company', 'companyBranch.manager'])
+        //     ->get()
+        //     ->map(function ($item) {
+        //         return [
+        //             'sc_id' => $item->sc_id,
+        //             'sc_student_id' => $item->sc_student_id,
+        //             'sc_company_id' => $item->sc_company_id,
+        //             'created_at' => $item->created_at,
+        //             'updated_at' => $item->updated_at,
+        //             'company' => [
+        //                 'c_id' => $item->company->c_id,
+        //                 'c_name' => $item->company->c_name,
+        //                 'created_at' => $item->company->created_at,
+        //                 'updated_at' => $item->company->updated_at,
+        //             ],
+        //             'company_branch' => [
+        //                 'b_id' => $item->companyBranch->b_id,
+        //                 'manager' => [
+        //                     'u_id' => $item->companyBranch->manager->u_id,
+        //                     'name' => $item->companyBranch->manager->name,
+        //                 ],
+        //             ],
+        //         ];
+        //     });
+
 
         //i will change it when we need it
         // $selectedCompanyInfo = $companies->map(function ($company) {
