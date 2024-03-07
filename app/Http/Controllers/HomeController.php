34a4f3fed864f015746326f6ac98c5e05cc4452e
AbuMaraqa <maraqamohamad@outlook.sa>
2024-03-07 09:52:52 +0200

    nn

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,9 +26,5 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function privacy_and_policy(){
-        return view('project.admin.privacy_and_policy');
-    }
-
     // test
 }
