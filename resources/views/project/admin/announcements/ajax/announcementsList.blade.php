fe2fa52c3c95c8da6dd54dcc81a42b30eabeafde
unknown <nooralhaddadd@gmail.com>
2024-03-24 10:57:25 +0200

    announcementByNoor

--- resources/views/project/admin/announcements/ajax/announcementsList.blade.php
+++ resources/views/project/admin/announcements/ajax/announcementsList.blade.php
@@ -1,12 +1,13 @@
-<div id="showTable">
-            <div class="table-responsive">
+<script src="{{ asset('assets/js/select2/select2.full.min.js') }}"></script>
+<script src="{{ asset('assets/js/select2/select2-custom.js') }}"></script>
+
+<div class="table-responsive">
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th scope="col" style="display:none;">id</th>
                             <th scope="col">{{__('translate.announcement_title')}} {{-- عنوان الاعلان --}}</th>
-                            <th scope="col">{{__('translate.added_by')}}{{-- منشئ الاعلان  --}}</th>
-                            <th scope="col">{{__('translate.announcement_content')}}{{-- محتوى الاعلان --}}</th>
+                            <th scope="col">{{__('translate.a_added_by')}}{{-- منشئ الاعلان  --}}</th>
                             <th scope="col">{{__('translate.announcement_stutas')}}{{-- حالة الاعلان --}}</th>
                             <th scope="col">{{__('translate.Operations')}}</th>
                         </tr>
@@ -22,16 +23,15 @@
                                 <td style="display:none;">{{ $key->a_id }}</td>
                                 <td>{{$key->a_title}}</td>
                                 <td>{{$key->users->name}} </td>
-                                <td>{{$key->a_content}}</td>
                                 <td> <select class="js-example-basic-single col-sm-12" name="a_stutas_{{$key->a_id}}" id="a_stutas_{{$key->a_id}}" onchange="changeAnnouncementStutas({{$key}})">
-                                        <option @if($key->a_stutas==1) selected  @endif value="0">مفعل</option>
-                                        <option @if($key->a_stutas==0) selected  @endif value="1">غير مفعل</option>
+                                        <option @if($key->a_stutas==1) selected  @endif value="1">مفعل</option>
+                                        <option @if($key->a_stutas==0) selected  @endif value="0">غير مفعل</option>
                                      </select></td>
-                                <td><button class="btn btn-info" onclick="showAnnouncement({{ $key }})"><i class="fa fa-info"></i></button></td>
+                                <td><button class="btn btn-info" onclick='location.href="{{route("admin.announcements.edit",["id"=>$key->a_id])}}"'><i class="fa fa-info"></i></button>
+                                <button class="btn btn-primary" onclick="editAnnouncement({{ $key }})"><i class="fa fa-edit"></i></button></td>
                             </tr>
                         @endforeach
                     @endif
                 </tbody>
                 </table>
             </div>
-        </div>
