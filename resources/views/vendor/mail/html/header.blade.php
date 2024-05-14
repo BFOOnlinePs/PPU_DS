cc37af1d57ccdacaa3bd50fe8f253c392ad0b505
AbuMaraqa <maraqamohamad@outlook.sa>
2024-05-14 14:30:47 +0300

    nn

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img style="width: 200px;height: 400px" src="{{ asset('assets/images/notification-logo.png') }}" class="logo" alt="Laravel Logo">
+<img style="width: 200px !important;max-height: 300px !important;" src="{{ asset('assets/images/notification-logo.png') }}" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
