389136ba25583fc9200c1f22c4addcb3f303e43c
AbuMaraqa <maraqamohamad@outlook.sa>
2024-05-15 11:18:31 +0300

    Edittest

--- app/Notifications/NewResetPasswordNotification.php
+++ app/Notifications/NewResetPasswordNotification.php
@@ -43,8 +43,10 @@ class NewResetPasswordNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('To reset your password, click the following button:')
-                    ->action('Reset Password', url('password/reset',$this->token));
+                    ->line('You are receiving this email because we received a password reset request for your account.')
+                    ->action('Reset Password', url('password/reset',$this->token))
+                    ->line('This password reset link will expire in 60 minutes.')
+                    ->line('If you did not request a password reset, no further action is required.');
     }
 
     /**
