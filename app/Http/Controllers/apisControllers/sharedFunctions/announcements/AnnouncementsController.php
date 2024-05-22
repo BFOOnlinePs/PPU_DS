3ceb173829a052c244c1faa33b165ec1c393f577
Aseel-Mohtaseb <aseelamermohtaseb@gmail.com>
2024-05-22 15:44:56 +0300

    edit announcement image

--- app/Http/Controllers/apisControllers/sharedFunctions/announcements/AnnouncementsController.php
+++ app/Http/Controllers/apisControllers/sharedFunctions/announcements/AnnouncementsController.php
@@ -160,7 +160,7 @@ class AnnouncementsController extends Controller
         $validator = Validator::make($request->all(), [
             'title' => 'required',
             'content' => 'required',
-            'image' => 'image|mimes:jpg,jpeg,png,svg',
+            'image' => 'nullable|image|mimes:jpg,jpeg,png,svg',
             // 'target_group' => 'required',
             'is_image_deleted' => 'in:true,false'
         ], [
@@ -196,8 +196,8 @@ class AnnouncementsController extends Controller
         $announcement->a_content = $request->input('content');
         $announcement->a_target_group = $request->input('target_group');
 
-
-        if ($request->input('is_image_deleted')) {
+        //  return $request->input('is_image_deleted');
+        if ($request->input('is_image_deleted') == 'true') {
             $announcement->a_image = null;
         }
 
