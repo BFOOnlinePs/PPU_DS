bfd145e8361cafe7eb043fbeb2e64410ed1d2a6f
AbuMaraqa <maraqamohamad@outlook.sa>
2024-10-06 14:30:39 +0300

    asd

--- resources/views/project/admin/evaluations/ajax/evaluation_deatils_table.blade.php
+++ resources/views/project/admin/evaluations/ajax/evaluation_deatils_table.blade.php
@@ -4,10 +4,10 @@
             <th>اسم الطالب</th>
             <th>اسم التدريب</th>
             <th>اسم المشرف</th>
-            <th>تقييم المشرف</th>
             <th>اسم الشركة</th>
-            <th>تقييم الشركة</th>
             <th>التاريخ</th>
+            <th>تقييم المشرف</th>
+            <th>تقييم الشركة</th>
             <th>العلامة النهائية</th>
         </tr>
     </thead>
@@ -29,10 +29,10 @@
                     <td>{{ $key->users->name }}</td>
                     <td>{{ $key->courses->c_name }}</td>
                     <td>{{ $key->supervisor->name }}</td>
-                    <td>{{ $key->university_score }}</td>
                     <td>{{ $key->studentCompany->company->c_name }}</td>
-                    <td>{{ $key->company_score }}</td>
                     <td>{{ $key->created_at }}</td>
+                    <td>{{ $key->university_score }}</td>
+                    <td>{{ $key->company_score }}</td>
                     <td>100 / {{ $key->university_score + $key->company_score }}</td>
                 </tr>
             @endif
