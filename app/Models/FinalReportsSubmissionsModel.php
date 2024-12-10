d88ac2a39013d4507810f4b21b18e8c0ed0d6faa
Mohamad Maraqa <mohamadmaraqa@Mohamads-MacBook-Pro.local>
2024-12-10 09:46:38 +0200

    asd

---
+++ app/Models/FinalReportsSubmissionsModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FinalReportsSubmissionsModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'final_reports_submissions';
+}
