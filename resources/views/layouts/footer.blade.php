0790c9adbd56254a635b65c10c4d987e9e0f4faa
Mohamad Maraqa <mohamadmaraqa@Mohamads-MacBook-Pro.local>
2024-12-15 14:58:29 +0200

    asd

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-6 footer-copyright">
-          <p class="mb-0">Copyright 2024-2023 © BeFound Online PS All rights reserved.</p>
+          <a href="https://www.facebook.com/befoundonlineps" class="mb-0">BFO</a>
         </div>
-        <div class="col-md-6">
+        {{-- <div class="col-md-6">
           <p class="pull-right mb-0">Hand crafted & made with <i class="fa fa-heart font-secondary"></i></p>
-        </div>
+        </div> --}}
       </div>
     </div>
   </footer>
