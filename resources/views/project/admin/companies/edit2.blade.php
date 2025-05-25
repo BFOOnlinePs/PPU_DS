64dbabc049dcd299a4bb33bd291b622b991c33f3
Mohamad Maraqa <maraqamohamad@outlook.sa>
2025-05-25 11:02:44 +0300

    asd

--- resources/views/project/admin/companies/edit2.blade.php
+++ resources/views/project/admin/companies/edit2.blade.php
@@ -94,19 +94,19 @@
                         <input type="text" value="{{ $user->email }}" name="email2" class="form-control" />
                     </div>
                 </div>
-                <div class="col-md-6">
+                {{-- <div class="col-md-6">
                     <div class="form-group">
                         <label>كلمة المرور</label>
                         <input type="text" name="pw" type='password' class="form-control" />
                     </div>
-                </div>
-                {{-- <div class="col-md-6">
+                </div> --}}
+                <div class="col-md-6">
                     <div class="form-group">
                         <label>رقم الهاتف المحمول</label>
                         <input type="text" name="mobile" value="{{ $user->u_phone1 }}" placeholder="تخزن رقم الهاتف اذا كانت فارغة"
                             class="form-control" />
                     </div>
-                </div> --}}
+                </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label>الشخص المسؤول</label>
