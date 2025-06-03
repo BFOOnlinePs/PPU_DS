71e1769cb8ab83141eee353e50ec7828f7d5422a
AbuMaraqa <maraqamohamad@outlook.sa>
2025-06-03 10:32:47 +0300

    asd

--- app/Http/Controllers/project/admin/CompaniesController.php
+++ app/Http/Controllers/project/admin/CompaniesController.php
@@ -282,7 +282,7 @@ class CompaniesController extends Controller
                     'userName' => $request->mobile,
                 ]
             ]);
-            return 'mohaamd';
+
             $responseData = json_decode($response->getBody(), true);
 
             if (!isset($responseData['success']) || !$responseData['success']) {
